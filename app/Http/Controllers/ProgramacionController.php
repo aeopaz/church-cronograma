19118f13cafe7f4c5346e7c23c2598ff2d0be421
Alvaro <edualvardo@hotmail>
2022-04-04 06:49:31 -0500

    Creacion Programa

--- app/Http/Controllers/ProgramacionController.php
+++ app/Http/Controllers/ProgramacionController.php
@@ -6,8 +6,10 @@ use App\Models\ParticipantesProgramacionMinisterio;
 use App\Models\Programacion;
 use App\Models\RecursoProgramacionMinisterio;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ProgramacionController extends Controller
 {
@@ -18,7 +20,7 @@ class ProgramacionController extends Controller
             ->join('programacions', 'programacions.id', 'programacion_id')
             ->join('rols', 'rols.id', 'rol_id')
             ->join('ministerios', 'ministerios.id', 'ministerio_id')
-            ->orderBy('programacions.fecha','desc')
+            ->orderBy('programacions.fecha', 'desc')
             ->get([
                 'programacion_id',
                 'ministerio_id',
@@ -35,12 +37,12 @@ class ProgramacionController extends Controller
     //Traer los participantes de un sólo programa
     public function participantes_program($programacion_id)
     {
-        
+
         $data = ParticipantesProgramacionMinisterio::join('programacions', 'programacions.id', 'programacion_id')
             ->join('rols', 'rols.id', 'rol_id')
             ->join('ministerios', 'ministerios.id', 'ministerio_id')
             ->join('users', 'users.id', 'participantes_programacion_ministerios.user_id')
-            ->where('programacion_id',$programacion_id)
+            ->where('programacion_id', $programacion_id)
             ->get([
                 'programacion_id',
                 'ministerio_id',
@@ -52,40 +54,64 @@ class ProgramacionController extends Controller
             ]);
         return response()->json(compact('data'));
     }
-     //Traer los recursos de un sólo programa
-     public function recursos_program($programacion_id)
-     {
-         
-        $data= RecursoProgramacionMinisterio::join('programacions', 'programacions.id', 'programacion_id')
-             ->join('ministerios', 'ministerios.id', 'ministerio_id')
-             ->join('recursos', 'recursos.id', 'recurso_id')
-             ->join('tipo_recursos', 'tipo_recursos.id', 'tipo_recurso_id')
-             ->where('programacion_id',$programacion_id)
-             ->get([
-                 'programacion_id',
-                 'ministerios.id as ministerio_id',
-                 'ministerios.nombre as nombre_ministerio',
-                 'recurso_id',
-                 'tipo_recursos.nombre as tipo_recurso',
-                 'recursos.nombre as nombre_recurso',
-             ]);
-        
-         return response()->json(compact('data'));
-     }
-      //Traer los programas que ha creado el usuario autenticado
-    public function own_program()
+    //Traer los recursos de un sólo programa
+    public function recursos_program($programacion_id)
     {
-        $data = User::find(Auth::user()->id)->programacionPropia()->
-        join('tipo_programacions','tipo_programacions.id','tipo_programacion_id')->get(
-            ['programacions.id as programacion_id',
-            'tipo_programacion_id',
-            'tipo_programacions.nombre as nombre_tipo_programacion',
-            'programacions.nombre as nombre_programa',
-            'programacions.fecha as fecha_programa',
-            'programacions.hora as hora_programa',]
 
-        );
+        $data = RecursoProgramacionMinisterio::join('programacions', 'programacions.id', 'programacion_id')
+            ->join('ministerios', 'ministerios.id', 'ministerio_id')
+            ->join('recursos', 'recursos.id', 'recurso_id')
+            ->join('tipo_recursos', 'tipo_recursos.id', 'tipo_recurso_id')
+            ->where('programacion_id', $programacion_id)
+            ->get([
+                'programacion_id',
+                'ministerios.id as ministerio_id',
+                'ministerios.nombre as nombre_ministerio',
+                'recurso_id',
+                'tipo_recursos.nombre as tipo_recurso',
+                'recursos.nombre as nombre_recurso',
+            ]);
+
         return response()->json(compact('data'));
     }
+    //Traer los programas que ha creado el usuario autenticado
+    public function own_program()
+    {
+        $data = User::find(Auth::user()->id)->programacionPropia()->join('tipo_programacions', 'tipo_programacions.id', 'tipo_programacion_id')->orderBy('programacions.fecha', 'desc')->get(
+                [
+                    'programacions.id as programacion_id',
+                    'tipo_programacion_id',
+                    'tipo_programacions.nombre as nombre_tipo_programacion',
+                    'programacions.nombre as nombre_programa',
+                    'programacions.fecha as fecha_programa',
+                    'programacions.hora as hora_programa',
+                ]
 
+            );
+        return response()->json(compact('data'));
+    }
+    //Crear una agenda o programacion
+    public function store(Request $request)
+    {
+        $fechaActual = Carbon::now();
+        $validator = Validator::make($request->all(), [
+            'tipo_programacion_id' => 'required|numeric',
+            'nombre' => 'required|string|max:60',
+            'fecha' => 'required|after_or_equal:' . $fechaActual,
+            'hora' => 'required|date_format:H:i'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $programacion = Programacion::create([
+            'tipo_programacion_id' => $request->tipo_programacion_id,
+            'iglesia_id' => 1,
+            'nombre' => $request->nombre,
+            'user_id' => Auth::user()->id,
+            'fecha' => $request->fecha,
+            'hora' => $request->hora,
+        ]);
+
+        return response()->json(compact('programacion'), 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -34,10 +34,17 @@ Route::group(['middleware' => 'jwt.verify'], function () {
     Route::post('/users/logout', [UserController::class, 'logout']);
     Route::get('/iglesias/index', [IglesiaController::class, 'index']);
 
+    Route::post('/program/store', [ProgramacionController::class,'store']);
     Route::get('/program/user_program', [ProgramacionController::class,'user_program']);
     Route::get('/program/participantes_program/{programa_id}', [ProgramacionController::class,'participantes_program']);
     Route::get('/program/recursos_program/{programa_id}', [ProgramacionController::class,'recursos_program']);
     Route::get('/program/own_program', [ProgramacionController::class,'own_program']);
+    
+
+    Route::get('/tipo_program/index', function(){
+        $data=TipoProgramacion::all(['id','nombre']);
+        return response()->json(compact('data'));
+    });
 });
 
 
